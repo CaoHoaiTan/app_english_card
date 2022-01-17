7ba6282d042c2302552c6b7824cdd91bf8e2ee78
CaoHoaiTan <caohoaitan@gmail.com>
2022-01-17 16:43:38 +0700

    Finish HomePage

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -11,6 +11,16 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int _currentIndex = 0;
+  late PageController _pageController;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    _pageController = PageController(viewportFraction: 0.9);
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
@@ -30,7 +40,7 @@ class _HomePageState extends State<HomePage> {
         ),
       ),
       body: Container(
-        margin: const EdgeInsets.symmetric(horizontal: 24),
+        // margin: const EdgeInsets.symmetric(horizontal: 24),
         width: double.infinity,
         child: Column(
           children: [
@@ -45,68 +55,105 @@ class _HomePageState extends State<HomePage> {
               ),
             ),
             Container(
+              // padding: const EdgeInsets.symmetric(horizontal: 24),
               height: size.height * 2 / 3,
               // page view để vuốt ngang
               child: PageView.builder(
+                  controller: _pageController,
+                  onPageChanged: (index) {
+                    setState(() {
+                      _currentIndex = index;
+                    });
+                  },
                   itemCount: 5,
                   itemBuilder: (context, index) {
-                    return Container(
-                      decoration: BoxDecoration(
-                          color: AppColors.primaryColor,
-                          // ĐỘ cong viền
-                          borderRadius: BorderRadius.all(Radius.circular(24))),
+                    return Padding(
+                      padding: const EdgeInsets.all(4.0),
                       child: Container(
-                        padding: const EdgeInsets.only(left: 16),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Container(
-                                padding:
-                                    const EdgeInsets.only(top: 16, right: 16),
-                                alignment: Alignment.centerRight,
-                                child: Image.asset(AppAssets.heart)),
-                            RichText(
-                                maxLines: 1,
-                                overflow: TextOverflow.ellipsis,
-                                textAlign: TextAlign.start,
-                                text: TextSpan(
-                                    text: 'B',
-                                    style: TextStyle(
-                                        fontFamily: FontFamily.sen,
-                                        fontSize: 89,
-                                        fontWeight: FontWeight.bold,
-                                        shadows: [
-                                          BoxShadow(
-                                              color: Colors.black38,
-                                              offset: Offset(3, 6),
-                                              blurRadius: 6)
-                                        ]),
-                                    children: [
-                                      TextSpan(
-                                        text: 'eautiful',
-                                        style: TextStyle(
-                                            fontFamily: FontFamily.sen,
-                                            fontSize: 56,
-                                            fontWeight: FontWeight.bold,
-                                            shadows: [
-                                              BoxShadow(
-                                                  color: Colors.black38,
-                                                  offset: Offset(3, 6),
-                                                  blurRadius: 6)
-                                            ]),
-                                      )
-                                    ])),
-                            Padding(
-                              padding: const EdgeInsets.only(top: 24),
-                              child: Text(
-                                "“Think of all the beauty still left around you and be happy.”",
-                                style: AppStyles.h4.copyWith(letterSpacing: 3),
-                              ),
-                            )
-                          ],
+                        padding: const EdgeInsets.all(16),
+                        // margin: const EdgeInsets.symmetric(vertical: 16),
+                        decoration: BoxDecoration(
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.black26,
+                                offset: Offset(3, 6),
+                                blurRadius: 6,
+                              )
+                            ],
+                            color: AppColors.primaryColor,
+                            // ĐỘ cong viền
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(24))),
+                        child: Container(
+                          padding: const EdgeInsets.only(left: 16),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Container(
+                                  padding:
+                                      const EdgeInsets.only(top: 16, right: 16),
+                                  alignment: Alignment.centerRight,
+                                  child: Image.asset(AppAssets.heart)),
+                              RichText(
+                                  maxLines: 1,
+                                  overflow: TextOverflow.ellipsis,
+                                  textAlign: TextAlign.start,
+                                  text: TextSpan(
+                                      text: 'B',
+                                      style: TextStyle(
+                                          fontFamily: FontFamily.sen,
+                                          fontSize: 89,
+                                          fontWeight: FontWeight.bold,
+                                          shadows: [
+                                            BoxShadow(
+                                                color: Colors.black38,
+                                                offset: Offset(3, 6),
+                                                blurRadius: 6)
+                                          ]),
+                                      children: [
+                                        TextSpan(
+                                          text: 'eautiful',
+                                          style: TextStyle(
+                                              fontFamily: FontFamily.sen,
+                                              fontSize: 56,
+                                              fontWeight: FontWeight.bold,
+                                              shadows: [
+                                                BoxShadow(
+                                                    color: Colors.black38,
+                                                    offset: Offset(3, 6),
+                                                    blurRadius: 6)
+                                              ]),
+                                        )
+                                      ])),
+                              Padding(
+                                padding: const EdgeInsets.only(top: 24),
+                                child: Text(
+                                  "“Think of all the beauty still left around you and be happy.”",
+                                  style: AppStyles.h4.copyWith(
+                                      letterSpacing: 3,
+                                      color: AppColors.textColor),
+                                ),
+                              )
+                            ],
+                          ),
                         ),
                       ),
                     );
+                  }),
+            ),
+            // Indicator
+            Container(
+              height: 12,
+              alignment: Alignment.center,
+              padding: const EdgeInsets.only(right: 24, left: 24),
+              margin: const EdgeInsets.symmetric(vertical: 8),
+              child: ListView.builder(
+                  physics: NeverScrollableScrollPhysics(),
+                  // Cuộn theo chiều ngang
+                  scrollDirection: Axis.horizontal,
+                  itemCount: 5,
+                  itemBuilder: (context, index) {
+                    return buildIndicator(index == _currentIndex, size);
                   }),
             )
           ],
@@ -117,6 +164,22 @@ class _HomePageState extends State<HomePage> {
         onPressed: () {},
         child: Image.asset(AppAssets.exchange),
       ),
+    );
+  }
+
+  Widget buildIndicator(bool isActive, Size size) {
+    return Container(
+      // height: 12,
+      margin: const EdgeInsets.symmetric(horizontal: 16),
+
+      width: isActive ? size.width * 1 / 5 : 24,
+      decoration: BoxDecoration(
+          color: isActive ? AppColors.lighBlue : AppColors.lightGrey,
+          borderRadius: BorderRadius.all(Radius.circular(12)),
+          boxShadow: [
+            BoxShadow(
+                color: Colors.black38, offset: Offset(2, 3), blurRadius: 3)
+          ]),
     );
   }
 }
