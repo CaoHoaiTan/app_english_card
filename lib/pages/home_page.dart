d5d06eca34361e6a1f2a0a27214fe9c7be40c2aa
CaoHoaiTan <caohoaitan@gmail.com>
2022-01-17 15:54:34 +0700

    HomePage2(PageVeiw)

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -13,6 +13,7 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
       backgroundColor: AppColors.secondColor,
       appBar: AppBar(
@@ -29,11 +30,85 @@ class _HomePageState extends State<HomePage> {
         ),
       ),
       body: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 24),
         width: double.infinity,
         child: Column(
           children: [
-            Text(
-                "It is amazing how complete is the delusion that beauty is goodness.")
+            Container(
+              height: size.height * 1 / 10,
+              padding: const EdgeInsets.all(16),
+              alignment: Alignment.centerLeft,
+              child: Text(
+                "It is amazing how complete is the delusion that beauty is goodness.",
+                style: AppStyles.h5
+                    .copyWith(fontSize: 12, color: AppColors.textColor),
+              ),
+            ),
+            Container(
+              height: size.height * 2 / 3,
+              // page view để vuốt ngang
+              child: PageView.builder(
+                  itemCount: 5,
+                  itemBuilder: (context, index) {
+                    return Container(
+                      decoration: BoxDecoration(
+                          color: AppColors.primaryColor,
+                          // ĐỘ cong viền
+                          borderRadius: BorderRadius.all(Radius.circular(24))),
+                      child: Container(
+                        padding: const EdgeInsets.only(left: 16),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Container(
+                                padding:
+                                    const EdgeInsets.only(top: 16, right: 16),
+                                alignment: Alignment.centerRight,
+                                child: Image.asset(AppAssets.heart)),
+                            RichText(
+                                maxLines: 1,
+                                overflow: TextOverflow.ellipsis,
+                                textAlign: TextAlign.start,
+                                text: TextSpan(
+                                    text: 'B',
+                                    style: TextStyle(
+                                        fontFamily: FontFamily.sen,
+                                        fontSize: 89,
+                                        fontWeight: FontWeight.bold,
+                                        shadows: [
+                                          BoxShadow(
+                                              color: Colors.black38,
+                                              offset: Offset(3, 6),
+                                              blurRadius: 6)
+                                        ]),
+                                    children: [
+                                      TextSpan(
+                                        text: 'eautiful',
+                                        style: TextStyle(
+                                            fontFamily: FontFamily.sen,
+                                            fontSize: 56,
+                                            fontWeight: FontWeight.bold,
+                                            shadows: [
+                                              BoxShadow(
+                                                  color: Colors.black38,
+                                                  offset: Offset(3, 6),
+                                                  blurRadius: 6)
+                                            ]),
+                                      )
+                                    ])),
+                            Padding(
+                              padding: const EdgeInsets.only(top: 24),
+                              child: Text(
+                                "“Think of all the beauty still left around you and be happy.”",
+                                style: AppStyles.h4.copyWith(letterSpacing: 3),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    );
+                  }),
+            )
           ],
         ),
       ),
